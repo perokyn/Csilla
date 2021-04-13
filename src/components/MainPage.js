95c8fd26bad8ddce1253c75ebf7656c3a17616ed
perokyn <perokyn@gmail.com>
2021-04-12 20:51:02 -0400

    initial commit

---
+++ src/components/MainPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardBody, Button, CardTitle, CardText, CardImg, Row, Col } from 'reactstrap';
+import Services from './services'
+
+
+const MainPage=()=>{
+
+return(
+
+<div>
+
+<Services></Services>
+
+</div>
+
+
+)
+
+
+
+}
+
+export default MainPage
