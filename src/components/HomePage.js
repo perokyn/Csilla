0ea472a1e900fd8e1fec2521158bd71999061a88
perokyn <perokyn@gmail.com>
2021-04-14 20:33:06 -0400

    added profile picture

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,56 +1,31 @@
 import React from 'react';
-import s1 from '../assets/images/s1.jpg'
-import s2 from '../assets/images/s2.jpg'
-import s3 from '../assets/images/s3.jpg'
+import { Media } from 'reactstrap';
+
+import p1 from '../assets/images/p1.jpg'
 
 
 import { Card, CardBody, Button, CardTitle, CardText, CardImg, Row, Col } from 'reactstrap';
+import Container from 'reactstrap/lib/Container';
 
 const Services = (props) => {
   return (
-    <Row className=' d-block d-sm-flex p-2'>
-        <Col>
-      <Card  >
-        <CardImg  top width="100%"   style={{ width: '7rem', margin:'auto' }}     src={s2} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Our Sellers</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">All our members are fully insured</small>
-          </CardText>
-        </CardBody>
-      </Card>
-      </Col>
 
 
-      <Col >
 
-      <Card  >
-        <CardImg top width="100%"   style={{ width: '7rem', margin:'auto' }}   src={s1} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Our Buyers</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">Direct client conenction</small>
-          </CardText>
-        </CardBody>
-      </Card>
-      </Col>
-      <Col>
+    <Media>
+    <Media right href="#">
+      
+    </Media>
+    <Media body>
+      <Media heading>
+        Media heading
+      </Media>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </Media>
+    <Media object  style={{width:'30rem', height:'30rem', borderRadius:'50%'}}    src={p1} alt="Generic placeholder image" />
+  </Media>
 
-      <Card>
-        <CardImg top width="100%"   style={{ width: '7rem', margin:'auto' }}   src={s3} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Our Agents</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">All our agents are certifyed</small>
-          </CardText>
-        </CardBody>
-      </Card>
 
-      </Col>
-    </Row>
   );
 };
 
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Card, CardBody, Button, CardTitle, CardText, CardImg, Row, Col } from 'reactstrap';
 import Services from './services'
 import NavBar from './Navbar'
-
+import HomePage from './HomePage'
 const MainPage=()=>{
 
 return(
 
 <div>
 <NavBar/>
+<HomePage/>
 <Services></Services>
 
 </div>
